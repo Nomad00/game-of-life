f6365fe601857efba7682036ae74eb1ac7bd70c3
Michael Stanziano <m.stanziano@gmail.com>
2022-06-20 13:54:37 -0400

    Initial commit.

---
+++ GameOfLife/GameOfLife/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by Michael Stanziano on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameOfLife/GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Michael Stanziano on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
